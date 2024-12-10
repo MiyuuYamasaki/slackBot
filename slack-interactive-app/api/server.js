fe2dcb410951b6c275ca9b7654c1f3999f04a6f8
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2024-12-10 11:03:59 +0900

    consolelog(envFile)

--- slack-interactive-app/api/server.js
+++ slack-interactive-app/api/server.js
@@ -10,6 +10,8 @@ const SLACK_TOKEN = process.env.SLACK_TOKEN;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY; // anon key
 
+console.log('SUPABASE_URL:', SUPABASE_URL);
+
 // クライアントの初期化
 const client = new WebClient(SLACK_TOKEN);
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY); // anon keyを使用
