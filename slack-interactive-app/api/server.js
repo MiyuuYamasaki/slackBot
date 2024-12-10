f018a7afc723bb613e316d7c5db8fc29110f7ab4
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2024-12-10 16:46:12 +0900

    step add forach

--- slack-interactive-app/api/server.js
+++ slack-interactive-app/api/server.js
@@ -48,6 +48,10 @@ app.post('/slack/actions', async (req, res) => {
       console.log('ymd_param:' + ymd);
       console.log('Data:', records);
 
+      records.foreach((record) => {
+        console.log(record.workStyle);
+      });
+
       if (queryError) throw queryError;
 
       // データを分類
