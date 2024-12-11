7000c92b6e5a5ea5541338c47a67fac78e91319c
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2024-12-11 17:12:21 +0900

    step7 chengeStyle

--- slack-interactive-app/api/server.js
+++ slack-interactive-app/api/server.js
@@ -353,7 +353,7 @@ app.post('/slack/actions', async (req, res) => {
                   emoji: true,
                 },
                 action_id: 'button_goHome',
-                style: leaveCheck % 2 === 0 ? 'default' : 'outline', // leaveCheck に基づいてボタンスタイルを変更
+                style: leaveCheck % 2 === 0 ? 'primary' : undefined,
               },
             ],
           },
